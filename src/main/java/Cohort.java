6299beb70d52f4c91bf75ae312f7f41903e93cfd
Carlos Teller <carlos.z.teller@gmail.com>
2020-04-09 16:57:38 -0500

    Completed the Testing Legacy Code exercise

--- src/main/java/Cohort.java
+++ src/main/java/Cohort.java
@@ -2,6 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cohort {
+//
+//    private List<Student> students;
+//
+//    public Cohort() {
+//        this.students = new ArrayList<>();
+//    }
+//
+//    public double getCohortAverage() {
+//        double avg = 0;
+//        for (Student student: this.getStudents()) {
+//            avg += student.getGradeAverage();
+//        }
+//        return avg / this.getStudents().size();
+//    }
+//
+//    public void addStudent(Student student){
+//        students.add(student);
+//    }
+//
+//    public List<Student> getStudents() {
+//        return students;
+//    }
 
     private List<Student> students;
 
@@ -9,19 +31,21 @@ public class Cohort {
         this.students = new ArrayList<>();
     }
 
-    public double getCohortAverage() {
-        double avg = 0;
-        for (Student student: this.getStudents()) {
-            avg += student.getGradeAverage();
-        }
-        return avg / this.getStudents().size();
+    public List<Student> getStudents() {
+        return students;
     }
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         students.add(student);
     }
 
-    public List<Student> getStudents() {
-        return students;
+    public double getCohortAverage() {
+        double cohortAverageSum = 0;
+
+        for(Student student : students) {
+            cohortAverageSum += student.getGradeAverage();
+        }
+
+        return cohortAverageSum / getStudents().size();
     }
 }
--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -6,40 +6,73 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class CohortTest {
+//
+//    Cohort emptyCohort;
+//    Cohort cohortWithOne;
+//    Cohort cohortWithMany;
+//
+//    @Before
+//    public void setup(){
+//        emptyCohort = new Cohort();
+//        cohortWithOne = new Cohort();
+//        cohortWithMany = new Cohort();
+//
+//        Student fer = new Student(1, "Fer M");
+//        fer.addGrade(90);
+//
+//        Student trant = new Student(2, "Trant B");
+//        trant.addGrade(100);
+//        trant.addGrade(100);
+//        trant.addGrade(100);
+//
+//        cohortWithOne.addStudent(fer);
+//        cohortWithMany.addStudent(fer);
+//        cohortWithMany.addStudent(trant);
+//    }
+//
+//    @Test
+//    public void testAddStudentAndGetStudentsWork(){
+//        assertEquals(0, emptyCohort.getStudents().size());
+//        assertEquals(1, cohortWithOne.getStudents().size());
+////        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+//    }
+//
+//    @Test
+//    public void testIfAvgIsCorrect(){
+//        assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+//    }
 
-    Cohort emptyCohort;
-    Cohort cohortWithOne;
-    Cohort cohortWithMany;
+    private Cohort fortuna;
 
     @Before
-    public void setup(){
-        emptyCohort = new Cohort();
-        cohortWithOne = new Cohort();
-        cohortWithMany = new Cohort();
+    public void setUp() {
+        fortuna = new Cohort();
 
-        Student fer = new Student(1, "Fer M");
-        fer.addGrade(90);
+        Student s1 = new Student(1, "S1");
+        Student s2 = new Student(1, "S2");
+        Student s3 = new Student(1, "S3");
 
-        Student trant = new Student(2, "Trant B");
-        trant.addGrade(100);
-        trant.addGrade(100);
-        trant.addGrade(100);
+        fortuna.addStudent(s1);
+        fortuna.addStudent(s2);
+        fortuna.addStudent(s3);
 
-        cohortWithOne.addStudent(fer);
-        cohortWithMany.addStudent(fer);
-        cohortWithMany.addStudent(trant);
+        s1.addGrade(75);
+        s1.addGrade(85);
+
+        s1.addGrade(80);
+        s1.addGrade(90);
+
+        s1.addGrade(85);
+        s1.addGrade(95);
     }
 
     @Test
-    public void testAddStudentAndGetStudentsWork(){
-        assertEquals(0, emptyCohort.getStudents().size());
-        assertEquals(1, cohortWithOne.getStudents().size());
-//        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+    public void testCohortNotEmpty() {
+        assertFalse("Expected cohort not to be empty.", fortuna.getStudents().isEmpty());
     }
 
     @Test
-    public void testIfAvgIsCorrect(){
-        assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+    public void testGetCohortAverage() {
+        assertEquals("Expected corhort average to be 85.0", 85.0, fortuna.getCohortAverage(), 0);
     }
-
 }
